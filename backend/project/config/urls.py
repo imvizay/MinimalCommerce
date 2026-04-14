689dd39e71742af79770c79da7ce1e77f4ed71f2
imvizay <vizzaymeena@gmail.com>
2026-04-14 19:30:41 +0530

    feat:Worked on building the product creation flow by adding support for images and variants in a single API request, and made sure proper validations are in place so the data stays clean. Also set up a router to handle routes in a more organized way instead of writing everything manually. Created a separate endpoint for users to view products without giving them access to admin-level actions. Updated the main urls file to include all the new routes, and registered the models in the admin panel so products, images, and variants can be managed easily from there.

--- backend/project/config/urls.py
+++ backend/project/config/urls.py
@@ -15,12 +15,26 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+
+# products routers 
+from apps.products.routers import router
+
+# Read Products
+from apps.products.views import ListProducts
 
 urlpatterns = [
     
     path('admin/', admin.site.urls),
+    path('api/products/',include(router.urls)),
+    
+  
+
+    path('api/auth/',include('apps.jwt_auth.urls')),
+    path('api/users/',include('apps.users.urls')),
+  
+  
 
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
