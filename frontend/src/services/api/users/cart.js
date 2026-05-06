40f605d80c77a9d72344db7646cfb84a1ff3c73a
imvizay <vizzaymeena@gmail.com>
2026-05-07 01:36:19 +0530

    Implemented optimistic cart quantity updates with debounced API syncing for authenticated users while keeping guest cart updates in localStorage.Refactored cart quantity mutation flow and backend PATCH endpoint to persist final quantity state directly into CartItem database records.

--- frontend/src/services/api/users/cart.js
+++ frontend/src/services/api/users/cart.js
@@ -9,3 +9,16 @@ export const saveCartIntoDb = (payload) => {
 export const loadCartFromDb = () => {
     return api.get('/cart/items/load_cart/')
 }
+
+// Remove Cart Item
+export const removeCartItemFromDb = (id) => {
+    return api.delete(`/cart/items/${id}/remove_item/`,)
+}
+
+// Update Cart Item
+export const updateCartItemQuantity = (id,quantity) => {
+    console.log("Running patch api endpoint",quantity)
+    return api.patch(`/cart/items/${id}/update_cart/`,{
+        quantity
+    })
+}
